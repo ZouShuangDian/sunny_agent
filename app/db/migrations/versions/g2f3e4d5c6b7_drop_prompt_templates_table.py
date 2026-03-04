53901e4ca751902f70759cedb53f5ac64341297e
zst <632371862@qq.com>
2026-03-04 15:10:13 +0800

    更新db关联

--- app/db/migrations/versions/g2f3e4d5c6b7_drop_prompt_templates_table.py
+++ app/db/migrations/versions/g2f3e4d5c6b7_drop_prompt_templates_table.py
@@ -14,7 +14,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'g2f3e4d5c6b7'
-down_revision: Union[str, None] = 'b2c3d4e5f6a7'
+down_revision: Union[str, None] = '9a8b7c6d5e4f'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
